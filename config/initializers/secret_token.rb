7fd50f22bd00ba27a048bedb1aaacd9d47171cd7
Lennon Shimokawa <lshimokawa@gmail.com>
2011-10-25 06:34:43 -0500

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitutils::Application.config.secret_token = '6287e7e0ad2c54d1b2738fb7a50d90e10f94e6f3d76e6addaa2d519b1a52d7795f496218f779f0569f9db23170f8b2accd16276f5c910b6c7033245a0565a35e'
